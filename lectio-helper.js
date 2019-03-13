cbfe611c75fd1afdb82d826ac57770742541139c
Scott Mathieson <scttmthsn@gmail.com>
2019-03-13 15:40:06 +0000

    Prettified

--- lectio-helper.js
+++ lectio-helper.js
@@ -1,4 +1,4 @@
-var exports = module.exports = {};
+var exports = (module.exports = {});
 /*exports.getWeekNumber = function(time){
     var d = new Date(time+6048e5);
     d.setHours(0,0,0);
@@ -6,41 +6,54 @@ var exports = module.exports = {};
     return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7)-1;
 };*/
 
-exports.getWeekNumber = function(time){
+exports.getWeekNumber = function (time) {
+  var target = new Date(time);
+  var dayNr = (target.getDay() + 6) % 7;
+  target.setDate(target.getDate() - dayNr + 3);
+  var firstThursday = target.valueOf();
+  target.setMonth(0, 1);
+  if (target.getDay() !== 4) {
+    target.setMonth(0, 1 + (4 - target.getDay() + 7) % 7);
+  }
+  var retVal = 1 + Math.ceil((firstThursday - target) / 604800000);
 
-	var target = new Date(time);
-    var dayNr = (target.getDay() + 6) % 7;
-    target.setDate(target.getDate() - dayNr + 3);
-    var firstThursday = target.valueOf();
-    target.setMonth(0, 1);
-    if (target.getDay() !== 4) {
-        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
-    }
-    var retVal = 1 + Math.ceil((firstThursday - target) / 604800000);
-  
-    return (retVal < 10 ? '0' + retVal : retVal);
-
-}
-
-exports.pad = function(arg){
-    if(String(arg).length==1) arg = "0" + arg; return arg;
+  return retVal < 10 ? '0' + retVal : retVal;
 };
 
-exports.dateFormat = function(date,skew){ //Skew compared to time in Denmark
-    if(skew!=0){
-        date.setTime(date.getTime()-3.6e6*skew);
-    }
-
-    return date.getUTCFullYear() +""+ this.pad(date.getUTCMonth() + 1) +""+ this.pad(date.getUTCDate()) + "T" + this.pad(date.getUTCHours()) +""+ this.pad(date.getUTCMinutes()) +""+ this.pad(date.getUTCSeconds()) + "Z";
+exports.pad = function (arg) {
+  if (String(arg).length == 1) arg = '0' + arg;
+  return arg;
 };
 
-exports.removePadding = function(str){
-	arr = str.split("\n");
-	arr.reverse();
-	while(arr[0]!=undefined && arr[0]==""){
-		arr.splice(0,1);
-	}
-	arr.reverse();
-	str = arr.join("\n");
-	return str;
+exports.dateFormat = function (date, skew) {
+  //Skew compared to time in Denmark
+  if (skew != 0) {
+    date.setTime(date.getTime() - 3.6e6 * skew);
+  }
+
+  return (
+    date.getUTCFullYear() +
+    '' +
+    this.pad(date.getUTCMonth() + 1) +
+    '' +
+    this.pad(date.getUTCDate()) +
+    'T' +
+    this.pad(date.getUTCHours()) +
+    '' +
+    this.pad(date.getUTCMinutes()) +
+    '' +
+    this.pad(date.getUTCSeconds()) +
+    'Z'
+  );
+};
+
+exports.removePadding = function (str) {
+  arr = str.split('\n');
+  arr.reverse();
+  while (arr[0] != undefined && arr[0] == '') {
+    arr.splice(0, 1);
+  }
+  arr.reverse();
+  str = arr.join('\n');
+  return str;
 };
--- storage.js
+++ storage.js
@@ -1,13 +1,13 @@
-var exports = module.exports = {};
-var fs = require("fs");
-var file = "./lectio.json";
+var exports = (module.exports = {});
+var fs = require('fs');
+var file = './lectio.json';
 cache = require(file);
-exports.read = function(name){
-	return cache[name];
+exports.read = function (name) {
+  return cache[name];
 };
-exports.write = function(name,value,callback){
-	cache[name] = value;
-	return fs.writeFile(file, JSON.stringify(cache), "utf8", (err) => {
-          if (err) throw err;
-        });
+exports.write = function (name, value, callback) {
+  cache[name] = value;
+  return fs.writeFile(file, JSON.stringify(cache), 'utf8', (err) => {
+    if (err) throw err;
+  });
 };
