e685e002ec03271489a8f5d9d3c4f2d1f427a476
Herover <leon.l.a.nielsen@gmail.com>
2021-08-17 22:05:05 +0200

    Add retries to browser functions (#22)
    * Add retries to browser functions
    
    * Re-enable browser test

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const URL = 'http://icanhazip.com';
 const SCHOOL = '590';
 
 import browser from './browser';
+import retry from './retry';
 
 const { exit, stderr, stdout } = process;
 
@@ -15,5 +16,35 @@ const { exit, stderr, stdout } = process;
   } catch (error) {
     stderr.write(error.message);
     exit(1);
+  }
+
+  const expectedResult = 'yay'
+  const fnSuccess = () => expectedResult;
+  
+  const result = await retry(fnSuccess);
+
+  if (result !== expectedResult) {
+    stderr.write(`expected result ${expectedResult}, got ${result}`);
+    exit(1);
+  }
+
+  let tryTimes = 0;
+  const expectedRetries = 3;
+  try {
+    const fnFail = () => {
+      tryTimes ++;
+      throw new Error('some error');
+    }
+    await retry(fnFail, { maxTries: expectedRetries });
+  } catch (error) {
+    if (error.message !== `Gave up after ${expectedRetries} attempts`) {
+      stderr.write(error.message);
+      exit(1);
+    }
+
+    if (tryTimes !== expectedRetries) {
+      stderr.write(`expected to try ${expectedRetries} times, actually tried ${tryTimes} times`);
+      exit(1);
+    }
   }
 })();
